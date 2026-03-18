31a9b6b7d8c00a2057c4186ee861054a46522f6a
Francis Causing <franzcsng23@gmail.com>
2026-03-18 15:02:52 +0800

    [0025-ONPT] Fix Checkbox component UI: implement field/checkbox custom color

--- src/components/ui/checkbox/Checkbox.js
+++ src/components/ui/checkbox/Checkbox.js
@@ -1,11 +1,19 @@
 import styles from './Checkbox.module.scss'
 
-
-export default function Checkbox({label, value, name}){
-    return(
-        <div className={styles['checkbox-container']}>
-            <input type="checkbox" value={value && value}/>
-            <label>{label}</label>
-        </div>
-    )
+export default function Checkbox({ label, value, name, checked, onChange }) {
+  return (
+    <div className={styles['checkbox-container']}>
+      <label className={styles.checkboxLabel}>
+        <input
+          type="checkbox"
+          value={value}
+          name={name}
+          checked={checked}
+          onChange={onChange}
+        />
+        <span className={styles.customCheckbox}></span>
+        {label}
+      </label>
+    </div>
+  )
 }
