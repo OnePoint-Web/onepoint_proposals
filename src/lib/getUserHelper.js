044a7c35ef184c97f756bdf29aaf4d5b987b6143
Francis Causing <franzcsng23@gmail.com>
2026-04-13 12:30:09 +0800

    [0056-ONPT] Create getUserHelper to fetch logged in user  details

---
+++ src/lib/getUserHelper.js
@@ -0,0 +1,18 @@
+import { cookies } from "next/headers"
+import { jwtVerify } from "jose"
+
+export async function requireUser() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get("auth_token")?.value
+
+  if (!token) {
+    throw new Error("Unauthorized")
+  }
+
+  const { payload } = await jwtVerify(
+    token,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  )
+
+  return payload
+}
