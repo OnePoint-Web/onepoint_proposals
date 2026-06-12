ca00fe614053f49409fb0c5d019c8473a3c13735
Franzcsng <franzcsng23@gmail.com>
2026-06-12 09:59:00 +0800

    [0193-ONPT] Complete proposal pdf sections

--- src/app/pdf/proposals/[slug]/page.js
+++ src/app/pdf/proposals/[slug]/page.js
@@ -3,32 +3,38 @@ import CoverPage from "./components/CoverPage";
 import ExecutiveSummary from "./components/ExecutiveSummary";
 import GoalsAndObjectives from "./components/GoalsAndObjectives";
 import ProposedSolution from "./components/Solution";
+import OurTeam from "./components/OurTeam";
+import OurApproach from "./components/OurApproach";
+import ProposedBudget from "./components/ProposedBudget";
 import { getIndividualProposalBySlug } from "@/lib/proposals";
 
 export default async function ProposalPDF({ params }) {
-
   const { slug } = await params;
-
- const proposal = await getIndividualProposalBySlug(slug);
+  const proposal = await getIndividualProposalBySlug(slug);
 
   if (!proposal) {
-    return <div styles={{color: 'black'}}>Proposal not found</div>;
+    return <div style={{ color: 'black' }}>Proposal not found</div>;
   }
-  
-  console.log(proposal)
 
   return (
     <>
-      <div class={styles['pdf-bg']}></div>
-
+      <div className={styles['pdf-bg']} />
       <div className={styles["pdf-page"]}>
-        <CoverPage proposal={proposal}/>
-        <ExecutiveSummary proposal={proposal}/>
-        <GoalsAndObjectives proposal={proposal}/>
-        <ProposedSolution proposal={proposal}/>
+        <CoverPage proposal={proposal} />
+        <ExecutiveSummary proposal={proposal} />
+        <GoalsAndObjectives proposal={proposal} />
+        <ProposedSolution proposal={proposal} />
 
+        {proposal.selectedMembers?.length > 0 && (
+          <OurTeam proposal={proposal} />
+        )}
+
+        {proposal.timelines?.length > 0 && (
+          <OurApproach proposal={proposal} />
+        )}
+
+        <ProposedBudget proposal={proposal} />
       </div>
     </>
-    
   );
 }
